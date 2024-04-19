584187acb2f56e9b7efe61115534cb42a169edaf
Hojjat-hk <minhojat@gmail.com>
2024-04-19 11:17:18 +0330

    Update PhP File

--- Success.php
+++ Success.php
@@ -1,26 +1,30 @@
-<?php
-require_once("Classes/Location.php");
-Location::navigateIfUserHasState(false, "Login.php");
-?>
+ <?php
+ require_once("Classes/Location.php");
+ Location::navigateIfUserHasState(false, "Login.php");
+ ?>
 
 <!DOCTYPE html>
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>File Ocean - Registration Form</title>
+	    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<linK rel="stylesheet" type="text/css" href="Styles/Main/Font.css">
+		<link rel="stylesheet" type="text/css" href="Styles/Main/Main.css">
+		<link rel="stylesheet" type="text/css" href="Styles/Success/Main.css">
+		<title>File Ocean - Success</title>
 	</head>
 	<body>
-		<form action="<?php echo(basename(htmlspecialchars($_SERVER["PHP_SELF"]))); ?>" method="get">
-			<button id="logout-button" name="logout-button">Log out</button>
-		</form>
+		<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+		<script type="application/javascript" src="Scripts/Success/Main.js"></script>
 	</body>
 </html>
 
-<?php
-if (!isset($_GET["logout-button"]))
-	exit();
+ <?php
+ if (!isset($_GET["logout-button"]))
+ 	exit();
 
-session_unset();
-session_destroy();
+ session_unset();
+ session_destroy();
 
-Location::navigate("Login.php");
+ Location::navigate("Login.php");
